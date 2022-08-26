d32faee43706dcad9fff47a8d3b941d0887bb02e
Crisandolin Rumahorbo <crisandolin.rumahorbo@btpn.com>
2022-08-26 16:59:39 +0700

    first commit

---
+++ src/test/java/com/rumahorbo/keycloakdemo/KeycloakDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rumahorbo.keycloakdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
